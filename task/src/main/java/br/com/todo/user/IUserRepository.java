417c480c7f12ce5c2f22b965f3a504bcba43e6b9
Renan Freitas <120645756+RFreitasAnjos@users.noreply.github.com>
2024-11-21 00:18:13 -0300

    Add files via upload

---
+++ task/src/main/java/br/com/todo/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.todo.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
