002919cf7d6f97ca4c63abb4da54beba2792c058
Renan Freitas <120645756+RFreitasAnjos@users.noreply.github.com>
2024-11-21 17:11:49 -0300

    Update TaskController.java
    Tratando para quando o usuário não tiver permissão

--- task/src/main/java/br/com/todo/task/TaskController.java
+++ task/src/main/java/br/com/todo/task/TaskController.java
@@ -48,14 +48,22 @@ public class TaskController {
     }
     // http://localhost:8080/tasks/id
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel,@PathVariable UUID id, HttpServletRequest request){
+    public ResponseEntity update(@RequestBody TaskModel taskModel,@PathVariable UUID id, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
 
+        if(task == null) {
+            return Response.Entity.status(HttpStatus.BAD_REQUEST)
+                .body("Tarefa não localizada.")
+        }
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Usuário não tem permissão para alterar essa tarefa.");
+        }
+        
         Utils.copyNonNullPorperties(taskModel, task);
-
-
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
     }
 }
